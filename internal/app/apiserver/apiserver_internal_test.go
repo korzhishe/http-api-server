4518faf1a73c8eeacde303e0b2306789f277096f
A.Kozhemyakin <agkozhemyakin@gmail.com>
2019-12-03 08:30:44 +0300

    first part

---
+++ internal/app/apiserver/apiserver_internal_test.go
@@ -0,0 +1,17 @@
+package apiserver
+
+import "testing"
+
+import "net/http/httptest"
+
+import "net/http"
+
+import "github.com/stretchr/testify/assert"
+
+func TestAPIServer_HandleHello(t *testing.T)  {
+	s := New(NewConfig())
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/hello", nil)
+	s.handleHello().ServeHTTP(rec, req)
+	assert.Equal(t, rec.Body.String(), "Hello")
+}
